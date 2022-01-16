d235815dac07a8293b136edf76a7b9a30e06afd4
vajramsrujan <55774184+vajramsrujan@users.noreply.github.com>
2022-01-15 19:31:08 -0600

    SegNet update

--- PyTorch/segnet/testing.py
+++ PyTorch/segnet/testing.py
@@ -11,10 +11,16 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import torch
 
-for i in range(1,367): 
+# for i in range(1,367): 
     
-    image = Image.open('archive/images/' + str(i).zfill(3) + '.png').convert('L')
-    image.save('archive/greyscale_images/' + str(i).zfill(3) + '.png')
+#     image = Image.open('archive/images/' + str(i).zfill(3) + '.png').convert('L')
+#     image.save('archive/greyscale_images/' + str(i).zfill(3) + '.png')
 
-# image = Image.open('archive/masks/' + str(4).zfill(3) + '.png').convert('L')
-# im2arr = np.array(image)
+image = Image.open('archive/masks/' + "036" + '.png').convert('RGB')
+im2arr = np.array(image)
+
+im2arr[im2arr==1]=128
+im2arr[im2arr==2]=255
+
+arr2im = Image.fromarray(im2arr).convert('RGB')
+arr2im.save("036_coloredmask.png")
