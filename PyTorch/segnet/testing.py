09513e80fef87e1f608785506459e6072da0df2b
Hussaini Lab <vajramsrujan@gmail.com>
2022-01-20 15:48:22 -0500

    Update SegNet notebook

--- PyTorch/segnet/testing.py
+++ PyTorch/segnet/testing.py
@@ -16,11 +16,15 @@ import torch
 #     image = Image.open('archive/images/' + str(i).zfill(3) + '.png').convert('L')
 #     image.save('archive/greyscale_images/' + str(i).zfill(3) + '.png')
 
-image = Image.open('archive/masks/' + "036" + '.png').convert('RGB')
-im2arr = np.array(image)
+# image = Image.open('archive/flattened_masks/' + "036" + '.png')
+# im2arr = np.array(image)
+# arr2im = Image.fromarray(im2arr).convert('RGB')
+# arr2im.save("036_coloredmask.png")
 
-im2arr[im2arr==1]=128
-im2arr[im2arr==2]=255
-
-arr2im = Image.fromarray(im2arr).convert('RGB')
-arr2im.save("036_coloredmask.png")
+class_1 = class_2 = class_3 = 0
+for i in range(1,367):
+    image = Image.open('archive/flattened_masks/' +  str(i).zfill(3) + '.png')
+    imm2arr = np.array(image)
+    class_1 += len(np.where(imm2arr==0)[0])
+    class_2 += len(np.where(imm2arr==1)[0])
+    class_3 += len(np.where(imm2arr==2)[0]) 
